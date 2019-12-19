2aca7828fb85f85d28c79625e7e57f4cc4c02875
trevorleake <trevorjohnleake@gmail.com>
2019-12-19 11:07:31 -0800

    Add fadein & fadeout

--- craig_the_poet.py
+++ craig_the_poet.py
@@ -18,9 +18,9 @@ import argparse
 import os
 from shutil import copyfile
 
-from utils import makedir, clean_word, download_image_from_url, LogDecorator
+from utils import makedir, clean_word, download_image_from_url, LogDecorator, text_to_image
 from google_utils import find_entities, synthesize_text, transcribe_audio, interval_of, download_image, list_blobs
-from ffmpeg_utils import create_slideshow, add_audio_to_video, change_audio_speed, media_to_mono_flac, resize_image
+from ffmpeg_utils import create_slideshow, add_audio_to_video, change_audio_speed, media_to_mono_flac, resize_image, fade_in_fade_out
 
 from Scraper import Scraper
 from mutagen.mp3 import MP3
@@ -34,14 +34,6 @@ class DomainError(Exception):
 class NoEntitiesInTTS(Exception):
     pass
 
-'''
-def next_log_file(directory):
-    files = os.listdir(directory)
-    if files:
-        greatest_num = max([int(filename.replace('log-', '').replace('.txt', '')) for filename in files])
-        return f'log-{greatest_num+1}.txt'
-    return f'log-{0}.txt'
-'''
 
 def next_log_file(log_directory):
     files = os.listdir(log_directory)
@@ -51,54 +43,120 @@ def next_log_file(log_directory):
     return 'log-0.txt'
 
 
+def get_filenames(post_subdirectory):
+    return {
+        'image_dir': f'{post_subdirectory}/image',
+        'frame_dir': f'{post_subdirectory}/image/frame',
+        'relative_frame_dir': '../image/frame',
+
+        'post.txt': f'{post_subdirectory}/text/post.txt',
+        'entities.txt': f'{post_subdirectory}/text/entities.txt',
+
+        'tts-title.mp3': f'{post_subdirectory}/audio/tts-title.mp3',
+        'tts-title-rate-RATE.mp3': f'{post_subdirectory}/audio/tts-title-rate-RATE.mp3',
+        'tts-title-rate-RATE.flac': f'{post_subdirectory}/audio/tts-title-rate-RATE.mp3',
+
+        'tts-body.mp3': f'{post_subdirectory}/audio/tts-body.mp3',
+        'tts-body-rate-RATE.mp3': f'{post_subdirectory}/audio/tts-body-rate-RATE.mp3',
+        'tts-body-rate-RATE.flac': f'{post_subdirectory}/audio/tts-body-rate-RATE.flac',
+
+        'body-slideshow.mp4': f'{post_subdirectory}/video/body-slideshow.mp4',
+        'body-slideshow-with-audio.mp4': f'{post_subdirectory}/video/body-slideshow-with-audio.mp4',
+        'body-slideshow-with-audio-and-fades.mp4': f'{post_subdirectory}/video/body-slideshow-with-audio-and-fades.mp4',
+        'body-concat.txt': f'{post_subdirectory}/video/body-concat.txt',
+
+        'title-frame.jpg': f'{post_subdirectory}/image/frame/title-frame.jpg',
+        'title-frame-full.jpg': f'{post_subdirectory}/image/frame/title-frame-full.jpg',
+        'relative title-frame-full.jpg': f'../image/frame/title-frame-full.jpg',
+
+        'title-slideshow.mp4': f'{post_subdirectory}/video/title-slideshow.mp4',
+        'title-slideshow-with-audio.mp4': f'{post_subdirectory}/video/title-slideshow-with-audio.mp4',
+        'title-slideshow-with-audio-and-fades.mp4': f'{post_subdirectory}/video/title-slideshow-with-audio-and-fades.mp4',
+        'title-concat.txt': f'{post_subdirectory}/video/title-concat.txt',
+
+        'poem-concat.txt': f'{post_subdirectory}/video/poem-concat.txt',
+        'poem.mp4': f'{post_subdirectory}/video/poem.mp4'
+    }
+    pass
+
+
+
+def create_file_structure(post_subdirectory):
+    makedir(post_subdirectory)
+    makedir(f'{post_subdirectory}/audio')
+    makedir(f'{post_subdirectory}/image')
+    makedir(f'{post_subdirectory}/text')
+    makedir(f'{post_subdirectory}/video')
+    makedir(f'{post_subdirectory}/image/frame')
+
+
+
+
 def create_poetry(title, body):
     # Make directories to store files for post
     clean_title = clean_word(title)
     post_subdirectory = f'{POSTS_DIRECTORY}/{clean_title}'
-    makedir(post_subdirectory)
 
-
-    makedir(f'{post_subdirectory}/audio')
-    makedir(f'{post_subdirectory}/images')
-    makedir(f'{post_subdirectory}/text')
-    makedir(f'{post_subdirectory}/video')
+    # Create directories and filenames
+    create_file_structure(post_subdirectory)
+    file_map = get_filenames(post_subdirectory)
 
     # Write the post's full text to file
-    with open(f'{post_subdirectory}/text/post.txt', 'w') as f:
+    with open(file_map['post.txt'], 'w') as f:
         f.write(title + '\n')
         f.write(body)
 
     # Find entities in body and write to file for records
     entities = find_entities(body)
-    with open(f'{post_subdirectory}/text/entities.txt', 'w') as f:
+    with open(file_map['entities.txt'], 'w') as f:
         logging.info(f'Entities detected: {[e.name for e in entities]}')
         for entity in entities:
             f.write(str(entity))
 
     # TTS on both title and body
-    body_tts_audio = f'{post_subdirectory}/audio/body.mp3'
+    synthesize_text(
+        title,
+        file_map['tts-title.mp3'],
+        name='en-IN-Wavenet-B',
+        pitch=-1,
+        speaking_rate=0.8,
+    )
+
     synthesize_text(
         body,
-        body_tts_audio,
+        file_map['tts-body.mp3'],
         name='en-IN-Wavenet-B',
         pitch=-1,
         speaking_rate=0.8,
     )
 
     # Slow the TTS voice further
-    change_audio_speed(f'{post_subdirectory}/audio/body.mp3', .9, f'{post_subdirectory}/audio/body-90-percent.mp3')
+    title_rate = 0.9
+    change_audio_speed(
+        file_map['tts-title.mp3'],
+        title_rate,
+        file_map['tts-title-rate-RATE.mp3'].replace('RATE', str(title_rate))
+    )
+
+    body_rate = 0.9
+    change_audio_speed(
+        file_map['tts-body.mp3'],
+        body_rate,
+        file_map['tts-body-rate-RATE.mp3'].replace('RATE', str(body_rate))
+    )
 
     # Find audio length
-    audio = MP3(f'{post_subdirectory}/audio/body-90-percent.mp3')
-    audio_length = audio.info.length
-
-    # Setup for transcription
-    audio_filepath = f'{post_subdirectory}/audio/body-90-percent.mp3'
-    flac_audio_filepath = f'{post_subdirectory}/audio/body.flac'
+    title_audio = MP3(file_map['tts-title-rate-RATE.mp3'].replace('RATE', str(title_rate)))
+    title_audio_length = title_audio.info.length
+    body_audio = MP3(file_map['tts-body-rate-RATE.mp3'].replace('RATE', str(body_rate)))
+    body_audio_length = body_audio.info.length
 
     # Transcribe the audio to learn when words are said
-    media_to_mono_flac(audio_filepath, flac_audio_filepath)
-    transcription = transcribe_audio(flac_audio_filepath)
+    media_to_mono_flac(
+        file_map['tts-body-rate-RATE.mp3'].replace('RATE', str(body_rate)),
+        file_map['tts-body-rate-RATE.flac'].replace('RATE', str(body_rate)),
+    )
+    transcription = transcribe_audio(file_map['tts-body-rate-RATE.flac'].replace('RATE', str(body_rate)))
 
     # TODO: Probably don't toss out words we can detect in speech.. Make estimates
     entity_intervals = dict()
@@ -109,7 +167,7 @@ def create_poetry(title, body):
 
     entity_information = dict()
     for word, interval in entity_intervals.items():
-        image_filepath = download_image(word, f'{post_subdirectory}/images', f'{word}')
+        image_filepath = download_image(word, file_map['image_dir'], f'{word}')
 
         entity_information[word] = {
             'image_filepath': f'{image_filepath}',
@@ -117,17 +175,16 @@ def create_poetry(title, body):
         }
 
     # Copy to frames directory to record selections for video
-    makedir(f'{post_subdirectory}/images/frames')
     for word, info in entity_information.items():
-        resize_image(f'{post_subdirectory}/images/{info["image_filepath"]}', 1920, 1080, f'{post_subdirectory}/images/frames/{word}.jpg')
-#        copyfile(f'{post_subdirectory}/images/{info["image_filepath"]}', f'{post_subdirectory}/images/frames/{word}.jpg')
-
-    no_audio_output_filepath = f'{post_subdirectory}/video/no_audio_poem.mp4'
-    output_filepath = f'{post_subdirectory}/video/poem.mp4'
-    concat_filepath = f'{post_subdirectory}/video/concat.txt'
+        resize_image(f'{file_map["image_dir"]}/{info["image_filepath"]}', 1920, 1080, f'{file_map["frame_dir"]}/{word}.jpg')
 
     # Sort entities by occurance in the source text
-    entity_information_list = sorted(list(entity_information.items()), key=lambda x: body.index(' ' + x[0]))
+    def find_word(word, text):
+        try:
+            return body.index(' ' + word)
+        except ValueError:
+            return body.index(word)
+    entity_information_list = sorted(list(entity_information.items()), key=lambda x: find_word(x[0], body))
 
     # Create no audio slideshow
     image_intervals = []
@@ -140,7 +197,7 @@ def create_poetry(title, body):
         if i != len(entity_information)-1:
             end = entity_information_list[i+1][1]['interval'][0]
         else:
-            end = audio_length
+            end = body_audio_length
         image_intervals += [(name, start, end)]
 
 
@@ -149,14 +206,44 @@ def create_poetry(title, body):
 
     image_information = []
     for (word, start, end) in image_intervals:
-        image_information.append((word, start, end, f'../images/frames/{word}.jpg'))
+        image_information.append((word, start, end, f"{file_map['relative_frame_dir']}/{word}.jpg"))
 
     # Create slideshow
-    write_concat_file(concat_filepath, image_information)
-    create_slideshow(concat_filepath, no_audio_output_filepath)
+    write_concat_file(file_map['body-concat.txt'], image_information)
+    create_slideshow(file_map['body-concat.txt'], file_map['body-slideshow.mp4'])
 
     # Add audio to slideshow
-    add_audio_to_video(no_audio_output_filepath, audio_filepath, output_filepath)
+    add_audio_to_video(
+        file_map['body-slideshow.mp4'],
+        file_map['tts-body-rate-RATE.mp3'].replace('RATE', str(body_rate)),
+        file_map['body-slideshow-with-audio.mp4']
+    )
+
+    # Text to image the title & resize
+    text_to_image(title, file_map['title-frame.jpg'])
+    resize_image(file_map['title-frame.jpg'], 1920, 1080, file_map['title-frame-full.jpg'])
+
+    # Create title card slideshow
+    title_information = [('title', 0, title_audio_length + 1, file_map['relative title-frame-full.jpg'])]
+    write_concat_file(file_map['title-concat.txt'], title_information)
+    create_slideshow(file_map['title-concat.txt'], file_map['title-slideshow.mp4'])
+    add_audio_to_video(
+        file_map['title-slideshow.mp4'],
+        file_map['tts-title-rate-RATE.mp3'].replace('RATE', str(title_rate)),
+        file_map['title-slideshow-with-audio.mp4']
+    )
+
+    fade_length = 0.3
+    fade_in_fade_out(file_map['title-slideshow-with-audio.mp4'], fade_length, file_map['title-slideshow-with-audio-and-fades.mp4'])
+    fade_in_fade_out(file_map['body-slideshow-with-audio.mp4'], fade_length, file_map['body-slideshow-with-audio-and-fades.mp4'])
+
+    with open(file_map['poem-concat.txt'], 'w') as f:
+        f.write(f"file 'title-slideshow-with-audio-and-fades.mp4'\n")
+        f.write(f"file 'body-slideshow-with-audio-and-fades.mp4'\n")
+    command = f"ffmpeg -y -safe 0 -f concat -i {file_map['poem-concat.txt']} -vcodec copy -acodec copy {file_map['poem.mp4']}"
+    check_output(command, shell=True)
+
+
 
 @LogDecorator()
 def write_concat_file(concat_filepath, image_information):
@@ -165,6 +252,11 @@ def write_concat_file(concat_filepath, image_information):
         for (word, start, end, filepath) in image_information:
             f.write(f'file {filepath}\n')
             f.write(f'duration {end - start}\n')
+
+        # BUG: This is the source of some miss timing on images & corruption for YouTube
+        # Audio gets misaligned because this file is longer in video than expected
+        # Concating seems to advance audio if the track is null Need to set the track to not null
+        f.write(f'file {filepath}\n')
 
 
 @LogDecorator()
